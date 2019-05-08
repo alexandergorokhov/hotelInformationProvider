62f79b1e7ff5fc9b414ee3eca572bcc2b51f6a29
Alexander Gorokhov <Alexander.Gorokhov@assecor.de>
2019-05-08 16:55:47 +0200

    Random word generation, Work in progress

--- src/main/java/information/provider/rest/controller/HotelInformationController.java
+++ src/main/java/information/provider/rest/controller/HotelInformationController.java
@@ -15,23 +15,53 @@ public class HotelInformationController {
 
     @Autowired
     private HotelInformationService hotelInformationService;
+
     @GetMapping(value = "/hotels")
-    public ResponseEntity<List<Hotel>> getAllHotels(){
+    public ResponseEntity<List<Hotel>> getAllHotels() {
         List<Hotel> listHotels = hotelInformationService.findAll();
         ResponseEntity response = new ResponseEntity(listHotels, HttpStatus.OK);
         return response;
     }
 
-    @PostMapping(value = "/hotels")
-    public ResponseEntity<List<Hotel>> postHotel(@RequestBody Hotel hotel){
+    @PostMapping(value = "/hotel")
+    public ResponseEntity postHotel(@RequestBody Hotel hotel) {
         ResponseEntity response;
-        try{
+        try {
             hotelInformationService.save(hotel);
-        }catch (Exception e ){
-          response= new ResponseEntity(hotel, HttpStatus.INTERNAL_SERVER_ERROR);
-          return response;
+        } catch (Exception e) {
+            response = new ResponseEntity(hotel, HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
         }
-        response= new ResponseEntity(hotel, HttpStatus.OK);
+        response = new ResponseEntity(hotel, HttpStatus.OK);
         return response;
     }
+
+    @PostMapping(value = "/hotels")
+    public ResponseEntity postHotels(@RequestBody List<Hotel> hotels) {
+        ResponseEntity response;
+        try {
+            hotelInformationService.saveHotels(hotels);
+        } catch (Exception e) {
+            response = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
+        response = new ResponseEntity( HttpStatus.OK);
+        return response;
+
+    }
+
+    @PostMapping(value = "/activate")
+    public ResponseEntity postActivate() {
+        ResponseEntity response;
+        try {
+            hotelInformationService.initialize();
+        } catch (Exception e) {
+            response = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
+        response = new ResponseEntity( HttpStatus.OK);
+        return response;
+
+    }
+
 }
--- src/main/java/information/provider/rest/service/HotelInformationService.java
+++ src/main/java/information/provider/rest/service/HotelInformationService.java
@@ -1,13 +1,19 @@
 package information.provider.rest.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import information.provider.pojo.Hotel;
 import information.provider.rest.dao.HotelInformationDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
 @Service
 public class HotelInformationService {
+    private final String INIT_FILENAME = "words.txt";
 
     @Autowired
     private HotelInformationDao hotelInformationDao;
@@ -19,5 +25,31 @@ public class HotelInformationService {
 
     public void save(Hotel hotel) {
         hotelInformationDao.save(hotel);
+    }
+
+    public void saveHotels(List<Hotel> hotels) {
+
+        hotels.stream().forEach(hotelInformationDao::save);
+
+    }
+
+    public void initialize() {
+        ClassLoader classLoader = new HotelInformationService().getClass().getClassLoader();
+        File file = new File(classLoader.getResource(INIT_FILENAME).getFile());
+       // file.get
+        try {
+            parseJson();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void parseJson() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        String inputString = "{json goes here...}";
+        JsonNode json = mapper.readTree(inputString);
+        JsonNode searchResultsNode = json.get("searchResults");
+        JsonNode classifiedAdvertsNode = searchResultsNode.get("classifiedAdverts");
+        String someValue = classifiedAdvertsNode.asText();
     }
 }
